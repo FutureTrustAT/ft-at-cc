0b5a1e34ae995466e8be6c37cb1d1998e8b4e579
Philip Helger <philip@helger.com>
2019-01-11 15:08:02 +0100

    ctd

--- src/main/java/at/gv/brz/futuretrust/cc/servlet/PublicHTMLProvider.java
+++ src/main/java/at/gv/brz/futuretrust/cc/servlet/PublicHTMLProvider.java
@@ -20,9 +20,9 @@ import java.util.Locale;
 
 import javax.annotation.Nonnull;
 
-import com.helger.commons.string.StringHelper;
 import com.helger.css.property.ECSSProperty;
 import com.helger.html.hc.html.embedded.HCImg;
+import com.helger.html.hc.html.grouping.HCDiv;
 import com.helger.html.hc.html.metadata.HCHead;
 import com.helger.html.hc.html.metadata.HCStyle;
 import com.helger.html.hc.html.root.HCHtml;
@@ -31,10 +31,7 @@ import com.helger.html.hc.html.sections.HCFooter;
 import com.helger.html.hc.html.sections.HCH1;
 import com.helger.html.hc.html.textlevel.HCSpan;
 import com.helger.html.hc.impl.HCEntityNode;
-import com.helger.photon.basic.app.appid.RequestSettings;
-import com.helger.photon.basic.app.menu.IMenuItemPage;
 import com.helger.photon.core.app.context.ISimpleWebExecutionContext;
-import com.helger.photon.core.app.context.LayoutExecutionContext;
 import com.helger.photon.core.app.html.AbstractSWECHTMLProvider;
 import com.helger.photon.core.url.LinkHelper;
 import com.helger.web.scope.IRequestWebScopeWithoutResponse;
@@ -53,23 +50,27 @@ public class PublicHTMLProvider extends AbstractSWECHTMLProvider
   {
     final IRequestWebScopeWithoutResponse aRequestScope = aSWEC.getRequestScope ();
     final Locale aDisplayLocale = aSWEC.getDisplayLocale ();
-    final IMenuItemPage aMenuItem = RequestSettings.getMenuItem (aRequestScope);
-    final LayoutExecutionContext aLEC = new LayoutExecutionContext (aSWEC, aMenuItem);
     final HCHead aHead = aHtml.head ();
     final HCBody aBody = aHtml.body ();
 
-    // Add menu item in page title
-    aHead.setPageTitle (StringHelper.getConcatenatedOnDemand ("BRZ FutureTrust pilot client",
-                                                              " - ",
-                                                              aMenuItem.getDisplayText (aDisplayLocale)));
+    aHead.setPageTitle ("BRZ FutureTrust pilot client");
 
     // TODO
     aHead.addCSS (new HCStyle ("* { font-family: Arial, Helvetica, sans-serif; }" +
                                "footer { background-color: #ddd; padding: 1rem; }"));
-    aBody.addChild (new HCH1 ().addChild (new HCImg ().setSrc (LinkHelper.getURLWithContext ("/imgs/FutureTrust-Logo-1.png"))
-                                                      .addStyle (ECSSProperty.WIDTH, "200px"))
-                               .addChild (new HCSpan ().addChild ("BRZ pilot client")
-                                                       .addStyle (ECSSProperty.MARGIN_LEFT, "2rem")));
+    aBody.addChild (new HCH1 ().addChild (new HCImg ().setSrc (LinkHelper.getURLWithContext ("/imgs/FutureTrust-Logo-6.png"))
+                                                      .addStyle (ECSSProperty.WIDTH, "100px"))
+                               .addChild (new HCSpan ().addChild ("BRZ FutureTrust pilot client")
+                                                       .addStyle (ECSSProperty.MARGIN_LEFT, "2rem")
+                                                       .addStyle (ECSSProperty.VERTICAL_ALIGN, "top"))
+                               .addStyle (ECSSProperty.BORDER_BOTTOM, "solid 1px #ddd"));
+
+    aBody.addChild (new HCDiv ().addChild ("Left")
+                                .addStyle (ECSSProperty.WIDTH, "49%")
+                                .addStyle (ECSSProperty.DISPLAY, "inline-block"));
+    aBody.addChild (new HCDiv ().addChild ("right")
+                                .addStyle (ECSSProperty.WIDTH, "49%")
+                                .addStyle (ECSSProperty.DISPLAY, "inline-block"));
 
     aBody.addChild (new HCFooter ().addChild (HCEntityNode.newCopy ()).addChild (" 2019 BRZ GmbH"));
   }
